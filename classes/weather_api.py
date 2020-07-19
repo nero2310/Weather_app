a679263c9c40c2b72966f6dc422f8507e34f5ac5
nero2310 <nero2310@wp.pl>
2020-07-19 17:40:43 +0200

    Change in tests to properly pass api_link

--- classes/weather_api.py
+++ classes/weather_api.py
@@ -16,21 +16,23 @@ class CurrentWeather:
                                           .format(city_name,state_code,country,api_key)
     """
 
-    def __init__(self, api_key: AnyStr, city_name: AnyStr, state_code=None,country=None , allow_http=False,
-                 api_link="""api.openweathermap.org/data/2.5/weather?q={0},{1},{2}&appid={3}"""):
+    def __init__(self, api_key: AnyStr, city_name: AnyStr, state_code=None, country=None, allow_http=False,
+                 api_link="""https://api.openweathermap.org/data/2.5/weather?q={0},{1},{2}&appid={3}"""):
         self.api_key = api_key
         self.city_name = city_name
         self.country = country
         self.state_code = state_code  # Only for USA
         self.api_link = api_link.format(city_name, state_code, country, api_key)
         self._check_url(allow_http)
-        
+
     def _check_url(self, allow_http):
         """allow_http: if false and api_link start with http:// raise exception UnsafeAdress"""
         if self.api_link[:8] != "https://":
             if self.api_link[:4] == "http":
                 if self.api_link[:5] != "https" and allow_http is False:
                     raise UnsafeAdress("Use https instead of http")  # i should write custom_exception to now use http
+                if self.api_link[:5] != "https" and allow_http is True:
+                    return self.api_link
             self.api_link = "https://" + self.api_link
 
     def make_request(self):
