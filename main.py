2fa60c4ad522e76002c562cd49ee6619a3225d4b
nero2310 <nero2310@wp.pl>
2020-07-22 12:22:04 +0200

    Add base bootstrap templates for Flask render template

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,render_template
 
 # from classes.configuration_loader import Config
 from classes.configuration_loader import Config
@@ -22,9 +22,13 @@ def get_weather():
 
 
 @app.route("/weather/<city>")
-def weather(city):
-    weather_data = CurrentWeather(api_key, city).make_request()
-    return jsonify(weather_data.json())
+def weather_city(city):
+    weather_data = CurrentWeather(api_key, city).make_request().json()
+    return render_template("weather.html",content=weather_data)
 
+@app.route("/weather/<city>/<country>")
+def weather_city_country(city,country):
+    weather_data = CurrentWeather(api_key,city,country=country).make_request()
+    return jsonify(weather_data.json())
 
 app.run(debug=True)  # run flask server
