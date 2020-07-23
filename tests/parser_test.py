26f899e9a6d9cb5da56f57873d4f9136d207e132
nero2310 <nero2310@wp.pl>
2020-07-23 16:35:00 +0200

    Small changes in input form  and add test

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -9,3 +9,7 @@ def test_kelvins_to_cel():
     assert temp_converter(10, "kelvin", "fahrenheit") == -441.67
     assert temp_converter(10, "fahrenheit", "kelvin") == 260.93
     assert temp_converter("1,2", "celsius", "fahrenheit") == 34.16
+
+
+def test_country_names_conversion():  # toDO convert country shortcut to full country name
+    assert False == True
